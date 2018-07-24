149dc97ed4acc5f0c5c4c107dabc0f1432d3aecf
Roy Kwon <rkwon@e1z3r5p14.42.us.org>
2018-07-24 16:08:20 -0700

    clean

--- Parseltongue/00/00_deepthought.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-
-print 'The answer to the Ultimate Question, of Life, the Universe and Everything, is "42".'
--- Parseltongue/01/01_player.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-name = input("Hello hacker, what is your name?")
-print ("Welcome, " + name + "!")
